c8254d48e3f8335bed8a19a63ee2e60674e0595a
Bhargavimukkamalla <bhargavimukkamalla123@gmail.com>
2025-07-27 17:49:01 +0300

    DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,4 +1,3 @@
-📄 Example 1:
 public class DataTypes {
     public static void main(String[] args) {
         byte b = 10;
@@ -22,39 +21,4 @@ public class DataTypes {
         System.out.println("String: " + str);
     }
 }
-
-
-📄 Example 2 – Default Values of Data Types
-🔹 Source Code
-public class DefaultValues {
-    // Instance variables (get default values)
-    byte b;
-    short s;
-    int i;
-    long l;
-    float f;
-    double d;
-    char c;
-    boolean bool;
-    String str;
-
-    void printDefaults() {
-        System.out.println("byte: " + b);
-        System.out.println("short: " + s);
-        System.out.println("int: " + i);
-        System.out.println("long: " + l);
-        System.out.println("float: " + f);
-        System.out.println("double: " + d);
-        System.out.println("char: '" + c + "'");
-        System.out.println("boolean: " + bool);
-        System.out.println("String: " + str);
-    }
-
-    public static void main(String[] args) {
-        DefaultValues obj = new DefaultValues();
-        obj.printDefaults();
-    }
-}
-
-
 
